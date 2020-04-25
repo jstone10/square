52eb433e433a1793c8b61c25b41a726338065afc
Josh Stone <joshastone@gmail.com>
2020-04-24 19:34:18 -0700

    Update square_class.py
    rewrote constructor and added comments

--- square_class.py
+++ square_class.py
@@ -13,17 +13,38 @@ import re
 import sys
 
 
+
+# Square Class
+# Description: Object that represents a square
+# Invairants:
+#      * Side lengths are rounded to the nearest integer
+#      * Side lengths are always positive
+#      * Side lengths at least 0 and at most 10
+# Caught Exceptions:
+#      * instantiating a square with a rounded side length  > 10 or < 0
+#        defaults the length to 0
 class Square(object):
-   
+
+   	# I: int side_length
+   	# O: none
+   	# Does: instantiates square object
     def __init__(self, side_length):
-        try:
-            self.side_length = float(side_length)    
-        except:
-            self.side_length = 0
-   
+    	side_length = math.round(side_length)
+    	if side_length > 10 or side_length < 0:
+    		self.side_length = 0
+    	else: 
+    		self.side_length = side_length
+    
+    # I: none
+    # O: Int
+    # Does: Computes perimeter of square based on side_length
     def get_perimeter(self):
     	return 4 * self.side_length
 
+
+    # I: none
+    # O: Int
+    # Does: Computes area of square based on side_lenght
     def get_area(self):
     	return self.side_length ** 2
 
