5ab6f9ef010c27f2b84fcea4a733607dcfdf4a51
Josh Stone <joshastone@gmail.com>
2020-04-24 19:41:34 -0700

    Update square_class.py
    re-added try except block to constructor

--- square_class.py
+++ square_class.py
@@ -23,30 +23,37 @@ import sys
 # Caught Exceptions:
 #      * instantiating a square with a rounded side length  > 10 or < 0
 #        defaults the length to 0
+#      * instanting a sqaure with a side_length that is not a number
 class Square(object):
 
-   	# I: int side_length
-   	# O: none
-   	# Does: instantiates square object
+    # I: int side_length
+    # O: none
+    # Does: instantiates square object
     def __init__(self, side_length):
-    	side_length = math.round(side_length)
-    	if side_length > 10 or side_length < 0:
-    		self.side_length = 0
-    	else: 
-    		self.side_length = side_length
+
+        try:
+            side_length = float(side_length)    
+        except:
+            side_length = 0
+
+        side_length = math.round(side_length)
+        if side_length > 10 or side_length < 0:
+            self.side_length = 0
+        else: 
+            self.side_length = side_length
     
     # I: none
     # O: Int
     # Does: Computes perimeter of square based on side_length
     def get_perimeter(self):
-    	return 4 * self.side_length
+        return 4 * self.side_length
 
 
     # I: none
     # O: Int
     # Does: Computes area of square based on side_lenght
     def get_area(self):
-    	return self.side_length ** 2
+        return self.side_length ** 2
 
 
 if __name__ == '__main__':
